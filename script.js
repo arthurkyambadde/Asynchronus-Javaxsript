f9949e15ec1db19fbf3c72fe32731b2a38a0f056
iamarthur <arthurkyambadde9@gmail.com>
2022-06-26 22:07:13 +0300

    coding challenge

--- script.js
+++ script.js
@@ -229,8 +229,48 @@ const whereAmI = function (lat, lng) {
 
 // btn.addEventListener('click', whereAmI());
 
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector('.images');
+
 const createImage = function (imgPath) {
   return new Promise(function (resolve, reject) {
     const img = document.createElement('img');
-  });
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  }).catch(err => console.log(error));
 };
+
+let currentImg;
+
+createImage('./img//img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.error('image loaded');
+    return wait(4);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('./img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.error('image loaded');
+    return wait(4);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.error(err));
